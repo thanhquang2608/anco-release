74924a540f000cbf4ace32dc510190a9c810c14e
Vo Thanh Quang <thanhquang2608@gmail.com>
2015-11-05 09:45:10 +0700

    update

--- www/js/controllers/SurveyController.js
+++ www/js/controllers/SurveyController.js
@@ -960,7 +960,7 @@
     $scope.saveDealer = function (isValid) {
         $scope.submited = true;
 
-        if (!isValid || ($scope.dealer.AC_PC != '2' && $scope.dealerIndex < 0)) {
+        if (!isValid) {
             $ionicLoading.hide();
             $ionicLoading.show({ template: 'Dữ liệu nhập chưa đúng, vui lòng kiểm tra lại!\n', noBackdrop: true, duration: 2000 });
             return;
